60cee130b416684d3f64de7de44d8684406562c9
sam <juckrit072@gmail.com>
2020-12-07 20:58:51 +0700

    question 2

--- app/src/test/java/com/example/ookbeetest/Question2Test.java
+++ app/src/test/java/com/example/ookbeetest/Question2Test.java
@@ -1,27 +1,20 @@
 package com.example.ookbeetest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class Question2Activity extends AppCompatActivity {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question2);
-    }
+public class Question2Test {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-//        จงเขียน function ในภาษา Java ที่รับ string ที่เป็นช่วงของวันที่ 2 ช่วง
+    @Test
+    public void question2_1_NotOverlapped() {
+        //        จงเขียน function ในภาษา Java ที่รับ string ที่เป็นช่วงของวันที่ 2 ช่วง
 //        และ return boolean ที่บอกว่า
 //        ช่วงเวลาทั้งสองช่วง มีวันซ้อนทับกันหรือไม่
 //        โดยให้สมมติว่า string ของวันที่จะอยู่ในรูปแบบ
@@ -43,6 +36,7 @@ public class Question2Activity extends AppCompatActivity {
         //ผมคงทำไม่ได้ เพราะงั้นผมจะตอบในรูปแบบของ yyyyMMdd
 
 
+        //Not Overlapped
         String start1Str="20201207";
         String end1Str="20201214";
         String start2Str="20201215";
@@ -53,44 +47,47 @@ public class Question2Activity extends AppCompatActivity {
         Date end2 = null;
         try {
             start1 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            end1 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            start2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            end2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
+            end1 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(end1Str);
+            start2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start2Str);
+            end2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(end2Str);
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-
-//        String start1Str="20201207";
-//        String end1Str="20201214";
-//        String start2Str="20201215";
-//        String end2Str="20201230";
-//        Date start1 = null;
-//        Date end1 = null;
-//        Date start2 = null;
-//        Date end2 = null;
-        try {
-            start1 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            end1 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            start2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-            end2 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start1Str);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-
-        if (isOverlapping(start1,end1,start2,end2)){
-            ScreenUtils.INSTANCE.showToast(this,"Overlapping");
-        }else {
-            ScreenUtils.INSTANCE.showToast(this,"Not Overlapping");
-        }
-
+        assertFalse(isOverlapping(start1,end1,start2,end2));
 
     }
 
+    @Test
+    public void question2_2_Overlapped(){
+        //Overlapped
+        String start3Str="20201207";
+        String end3Str="20201214";
+        String start4Str="20201208";
+        String end4Str="20201230";
+        Date start3 = null;
+        Date end3 = null;
+        Date start4 = null;
+        Date end4 = null;
+        try {
+            start3 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start3Str);
+            end3 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(end3Str);
+            start4 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(start4Str);
+            end4 = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(end4Str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assertTrue(isOverlapping(start3,end3,start4,end4));
+
+
+    }
 
 
     public boolean isOverlapping(Date start1, Date end1, Date start2, Date end2) {
         return start1.before(end2) && start2.before(end1);
     }
+
+
+
+
 }

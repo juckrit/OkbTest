55c476a20a428be42391b8f4667cf005e3ba9cf3
sam <juckrit072@gmail.com>
2020-12-07 21:47:08 +0700

    import package

--- app/src/test/java/com/example/ookbeetest/Question3Test.java
+++ app/src/test/java/com/example/ookbeetest/Question3Test.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class Question3Test {
 

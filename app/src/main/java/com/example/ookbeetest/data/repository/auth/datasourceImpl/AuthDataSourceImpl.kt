359edef6f1967d817d316e45d883c7f32cd62fd2
sam <juckrit072@gmail.com>
2020-12-12 14:05:21 +0700

    use EncryptedSharedPreferences

--- app/src/main/java/com/example/ookbeetest/data/repository/auth/datasourceImpl/AuthDataSourceImpl.kt
+++ app/src/main/java/com/example/ookbeetest/data/repository/auth/datasourceImpl/AuthDataSourceImpl.kt
@@ -1,25 +1,37 @@
 package com.example.ookbeetest.data.repository.auth.datasourceImpl
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.example.ookbeetest.MY_PREFS_KEY_TOKEN
-import com.example.ookbeetest.MY_PREFS_NAME
 import com.example.ookbeetest.data.repository.auth.datasource.AuthDatasource
 
 
 class AuthDataSourceImpl(private val context: Context) :
     AuthDatasource {
+
+    var masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    var sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+        "secret_shared_prefs",
+        masterKeyAlias,
+        context,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
     override fun saveToken(token: String) {
-        val editor = context.getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit()
+        // use the shared preferences and editor as you normally would
+        var editor = sharedPreferences.edit()
         editor.putString(MY_PREFS_KEY_TOKEN, token)
         editor.apply()
     }
 
     override fun getToken(): String {
-        val prefs: SharedPreferences = context.getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE)
         val token =
-            prefs.getString(MY_PREFS_KEY_TOKEN, "") ?: ""
+            sharedPreferences.getString(MY_PREFS_KEY_TOKEN, "") ?: ""
         return token
     }
+
+
 }
